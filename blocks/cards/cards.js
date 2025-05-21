7d0944f93615bae17d0fc1201ddadf02585578b8
Vladyslav Osypchuk <vladyslav.osypchuk@sap.com>
2025-05-21 21:19:10 +0300

    dgl plugin, render with request

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,21 +3,15 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
+  let linkType;
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      const dglDiv = Array.from(div.children)?.find((child) => child.dataset?.aueProp === 'dgl');
-      if (dglDiv) {
-        try {
-          const jsonData = JSON.parse(dglDiv.innerText);
-          dglDiv.innerText = Object.values(jsonData).join('\n\n');
-        } catch (error) {
-          console.error('Invalid JSON in dglDiv:', error);
-        }
-      }
+      linkType = findLinkType(div, linkType);
+      renderDgl(div, linkType);
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
@@ -31,3 +25,37 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(ul);
 }
+
+function findLinkType(div, linkType) {
+  const linkTypeContainer = Array.from(div.children)?.find((child) => child.dataset?.aueProp === 'linkType');
+
+  if (linkTypeContainer) {
+    linkType = linkTypeContainer.innerText;
+  }
+
+  return linkType;
+}
+
+async function renderDgl(div, linkType) {
+  const dglDiv = Array.from(div.children)?.find((child) => child.dataset?.aueProp === 'dgl');
+  if (linkType === 'dgl' && dglDiv) {
+    try {
+      const url = new URL('https://16778-dgltest-stage.adobeio-static.net/api/v1/web/dgl/dglIdSearch');
+      url.searchParams.append('id', JSON.parse(dglDiv.innerText).id)
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      if (response.ok) {
+        const body = await response.json();
+        console.log(body);
+        dglDiv.innerText = body.approvedVersionDownloadUrl;
+      }
+    } catch (error) {
+      console.error('Invalid JSON in dglDiv:', error);
+    }
+  }
+}
+
